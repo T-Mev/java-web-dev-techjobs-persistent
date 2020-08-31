18a42f85f34bb6e6ef0a60077959ed416b9a6e65
Ted M <practice@example.com>
2020-08-30 22:15:07 -0400

    Completed Part 1

--- queries.sql
+++ queries.sql
@@ -1,7 +1,17 @@
+
 ## Part 1: Test it with SQL
+
+SELECT COLUMN_NAME, DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'techjobs' AND TABLE_NAME = 'job';
 
 ## Part 2: Test it with SQL
 
+
+
 ## Part 3: Test it with SQL
 
+
+
 ## Part 4: Test it with SQL
+
