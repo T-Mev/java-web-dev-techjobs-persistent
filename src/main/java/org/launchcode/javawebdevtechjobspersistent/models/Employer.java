93aec448ae219865bfcd609d3698d0e386f1f4c6
Ted M <practice@example.com>
2020-08-31 23:57:43 -0400

    Completed Part 3

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,8 +1,14 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import org.w3c.dom.stylesheets.LinkStyle;
+
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +16,10 @@ public class Employer extends AbstractEntity {
     @NotBlank(message = "Location required")
     @Size(min = 3, max = 50, message = "Location must be between 3 and 50 characters")
     private String location;
+
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer() { }
 
