af645661af27cf4e58536d8a09bbc7c22c03f235
Ted M <practice@example.com>
2020-09-02 22:05:41 -0400

    Completed Assignment

--- queries.sql
+++ queries.sql
@@ -1,19 +1,21 @@
 
 ## Part 1: Test it with SQL
-
 SELECT COLUMN_NAME, DATA_TYPE
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'techjobs' AND TABLE_NAME = 'job';
 
 ## Part 2: Test it with SQL
-
 SELECT name
 FROM employer
 WHERE location = "St. Louis";
 
 ## Part 3: Test it with SQL
-
 DROP TABLE job;
 
 ## Part 4: Test it with SQL
-
+SELECT name, description
+FROM skill AS s
+INNER JOIN job_skills AS j
+ON s.id = j.skills_id
+WHERE j.jobs_id IS NOT NULL
+ORDER BY name;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,13 +1,21 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank(message = "Description required")
     @Size(min = 3,max = 200, message = "Description must be between 3 and 200 characters")
     private String description;
+
+    @ManyToMany(mappedBy="skills")
+    private List<Job> jobs = new ArrayList<>();
 
     public Skill() { }
 
@@ -17,5 +25,9 @@ public class Skill extends AbstractEntity {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
     }
 }
